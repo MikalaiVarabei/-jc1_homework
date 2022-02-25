417edae18407d2eee8f1d66bce7a31a6394f47b8
Mikalai Varabei <7709215@gmail.com>
2022-02-25 17:57:32 +0300

    add metod task2

--- src/homework/one/HomeWorkOne.java
+++ src/homework/one/HomeWorkOne.java
@@ -1,4 +1,18 @@
 package homework.one;
 
 public class HomeWorkOne {
+
+
+    private void task3(){
+        byte b = 0x55;
+        short s = 0x55FF;
+        int i = 1_000_000;
+        long l = 0xFFFFFFFFL;
+        char c = 'a';
+        float f = .25F;
+        double d = .00001234;
+        boolean bool = true;
+    }
+
+
 }
