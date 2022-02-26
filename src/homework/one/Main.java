d2f1f8ee8b56457253d5799e3e3b40dd15207456
Mikalai Varabei <7709215@gmail.com>
2022-02-26 14:37:12 +0300

    task 6,7,8,9,10,11

--- src/homework/one/Main.java
+++ src/homework/one/Main.java
@@ -2,32 +2,49 @@ package homework.one;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("task3:");
+        System.out.println("********** task3: **********");
+        //обьявление переменных
         new One().task3();
+        System.out.println(" ");
 
-        System.out.println("task4:");
+        System.out.println("********** task4: **********");
+        //сумма, произведение
         System.out.println(new One().task4(2, 3));
+        System.out.println(" ");
 
-        System.out.println("task5:");
+        System.out.println("********** task5: **********");
+        //секунды
         new One().task5();
+        System.out.println(" ");
 
-        System.out.println("task6:");
-        new One().task6();
+        System.out.println("********** task6: **********");
+        // выводим последнюю цифру
+        System.out.println(new One().task6(1777771));
+        System.out.println(" ");
 
-        System.out.println("task7:");
-        new One().task7();
+        System.out.println("********** task7: **********");
+        // длина, ширина, радиус
+        new One().task7(3, 4, 4);
+        System.out.println(" ");
 
-        System.out.println("task8:");
-        new One().task8();
+        System.out.println("********** task8: **********");
+        // рубли
+        new One().task8(110013);
+        System.out.println(" ");
 
-        System.out.println("task9:");
-        new One().task9();
+        System.out.println("********** task9: **********");
+        //день, месяц, год
+        new One().task9(31, 12, 2020);
+        System.out.println(" ");
 
-        System.out.println("task10:");
-        new One().task10();
+        System.out.println("********** task10: **********");
+        //дом-1, дом-2, участок
+        new One().task10(3,4, 3,7, 4,10);
+        System.out.println(" ");
 
-        System.out.println("task11:");
-        new One().task11();
-
+        System.out.println("********** task11: **********");
+        // дни недели 0..6
+        new One().task11(2);
+        System.out.println(" ");
     }
 }
--- src/homework/one/One.java
+++ src/homework/one/One.java
@@ -1,6 +1,7 @@
 package homework.one;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class One {
     public void task3(){
@@ -24,6 +25,8 @@ public class One {
     }
 
     public int task4(int a, int b){
+        //без проверок на переполнение!
+        System.out.println("(a+b)+(a*b)=");
         int sum = a+b;
         int mul = a*b;
         return sum+mul;
@@ -31,39 +34,207 @@ public class One {
 
     public void task5(){
         long timeSec = 1_000_000;
-        double timeMin = timeSec/60D;
-        double timeHour = timeSec/(60*60D);
-        double timeDay = timeSec/(60*60*24D);
-        double timeWeek = timeSec/(60*60*24*7D);
+        double timeMin = (double)(timeSec)/60;
+        double timeHour = (double)(timeSec)/(60*60);
+        double timeDay = (double)(timeSec)/(60*60*24);
+        double timeWeek = (double)(timeSec)/(60*60*24*7);
 
-        System.out.println("a "+timeSec+" seconds is");
-        System.out.println(timeMin + " minutes, or");
-        System.out.println(timeHour + " hour, or");
-        System.out.println(timeDay + " day, or");
-        System.out.println(timeWeek + " week");
+        System.out.println("в "+timeSec+" секунд(ах)");
+        System.out.println(timeMin + " минут, или");
+        System.out.println(timeHour + " часов, или");
+        System.out.println(timeDay + " дней, или");
+        System.out.println(timeWeek + " недель");
     }
 
-    public void task6(){
-
+    public boolean task6(int i){
+        int ld = i%10;
+        System.out.println(i+", последняя цыфра: "+ld);
+        return (ld == 7);
     }
 
-    public void task7(){
+    public void task7(int a, int b, int r){
+        //радиус описанной окружности для четырехугольника R = SQRT(a^2 + b^2)/2
+        double rc = Math.sqrt((double)(a*a) + (double)(b*b))/2;
+        System.out.println("задан радиус окружности: " + r + ", радиус описанной окружности: " + rc);
 
+        if(r >= rc) {
+            System.out.println("круг перекрывает четырехугольник");
+            return;
+        }
+
+        System.out.println("круг не перекрывает четырехугольник");
     }
 
-    public void task8(){
+    public void task8(int n){
+        int lastDigit = n%10;           //последний разряд
+        int penLastDigit = (n%100)/10;  //предпоследний разряд (для определения от 10 до 19)
+        //System.out.println(penLastDigit);
 
+        if(penLastDigit == 1)                                           // оканчивается на 10..19
+            System.out.println(n+" рублей");
+        else if(lastDigit == 1)                                         // оканчивается на 1
+            System.out.println(n+" рубль");
+        else if ((lastDigit == 2)||(lastDigit == 3)||(lastDigit == 4))  // оканчивается на 2..4
+            System.out.println(n+" рубля");
+        else if ((lastDigit == 0)||(lastDigit >= 5))                    // оканчивается на 0, 5..9
+            System.out.println(n+" рублей");
     }
 
-    public void task9(){
+    public void task9(int day, int month, int year){
+        int upDay = 1;//0..28!!!
 
+        Calendar calendar = new GregorianCalendar(year, (month-1), day);
+        int maxDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);  //количесво дней в заданном месяце
+
+        System.out.println("введена дата " + day + "." + month + "." + year);
+        System.out.println(maxDay + " дней в заданном месяце");
+        System.out.println(upDay + " дней прибавили");
+
+        int nextDay = day+upDay;
+        int nextMont = month;
+        int nextYear = year;
+
+        if(nextDay > maxDay) {
+            nextDay -= maxDay;
+            nextMont += 1;
+            if(nextMont > 12) {
+                nextMont -= 12;
+                nextYear++;
+            }
+        }
+
+        System.out.println("грядущая дата " + nextDay + "." + nextMont + "." + nextYear);
     }
 
-    public void task10(){
+    public void task10(int a, int b, int c, int d, int e, int f){
+        System.out.println("дом-1: "+a+"x"+b);
+        System.out.println("дом-2: "+c+"x"+d);
+        System.out.println("участок: "+e+"x"+f);
 
+        int count=0;
+        // размещаем дома вплотную
+        // поворачиваем по одному,
+        // проверяем вписываются ли размеры в периметр участка
+        // есть 4 способа разместить дома
+
+        //    A         C
+        //  +----+   +------+
+        // B|  1 |  D|  2   |
+        //  +----+   +------+
+        if((a+c)<=e) {          // сравниваем с длиной участка
+            if((b <= f)&&(d <= f)){
+                count++;
+                System.out.println("способ-1 позволяет разместить");
+            }
+        }
+        //"поворачиваем" участок на 90 град.
+        else if((a+c)<=f) {     //сравниваем с шириной участка
+            if((b <= e)&&(d <= e)){
+                count++;
+                System.out.println("способ-1.1 позволяет разместить");
+            }
+        }
+
+        //    B         C
+        //  +----+   +------+
+        // A|  1 |  D|  2   |
+        //  +----+   +------+
+        if((b+c)<=e) {          // сравниваем с длиной участка
+            if((a <= f)&&(d <= f)){
+                count++;
+                System.out.println("способ-2 позволяет разместить");
+            }
+        }
+        //"поворачиваем" участок на 90 град.
+        else if((b+c)<=f) {     //сравниваем с шириной участка
+            if((a <= e)&&(d <= e)){
+                count++;
+                System.out.println("способ-2.1 позволяет разместить");
+            }
+        }
+
+        //    A         D
+        //  +----+   +------+
+        // B|  1 |  C|  2   |
+        //  +----+   +------+
+        if((a+d)<=e) {          // сравниваем с длиной участка
+            if((b <= f)&&(c <= f)){
+                count++;
+                System.out.println("способ-3 позволяет разместить");
+            }
+        }
+        //"поворачиваем" участок на 90 град.
+        else if((a+d)<=f) {     //сравниваем с шириной участка
+            if((b <= e)&&(c <= e)){
+                count++;
+                System.out.println("способ-3.1 позволяет разместить");
+            }
+        }
+
+        //    B         D
+        //  +----+   +------+
+        // A|  1 |  C|  2   |
+        //  +----+   +------+
+        if((b+d)<=e) {          // сравниваем с длиной участка
+            if((a <= f)&&(c <= f)){
+                count++;
+                System.out.println("способ-4 позволяет разместить");
+            }
+        }
+        //"поворачиваем" участок на 90 град.
+        else if((b+d)<=f) {     //сравниваем с шириной участка
+            if((a <= e)&&(c <= e)){
+                count++;
+                System.out.println("способ-4.1 позволяет разместить");
+            }
+        }
+
+        if(count > 0)
+            System.out.println("есть "+count+" способ(а) разместить дома на учаске");
+        else
+            System.out.println("дома нельзя разместить на данном участке");
     }
 
-    public void task11(){
+    public void task11(int n){
+        switch (n){
+            case 1:
+                System.out.println("Понедельник");
+                System.out.println("работа-дом");
+                break;
 
+            case 2:
+                System.out.println("Вторник");
+                System.out.println("работа-учеба-дом");
+                break;
+
+            case 3:
+                System.out.println("Среда");
+                System.out.println("работа-дом");
+                break;
+
+            case 4:
+                System.out.println("Четверг");
+                System.out.println("работа-учеба-дом");
+                break;
+
+            case 5:
+                System.out.println("Пятница");
+                System.out.println("работа-дом");
+                break;
+
+            case 6:
+                System.out.println("Суббота");
+                System.out.println("уроки, кинотеатр");
+                break;
+
+            case 0:
+                System.out.println("Воскресенье");
+                System.out.println("ничегонеделание");
+                break;
+
+            default:
+                System.out.println("Ведено неверное число");
+                break;
+        }
     }
 }
