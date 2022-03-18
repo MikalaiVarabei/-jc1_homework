5bc03ced8ece9c66fa92966ad41044034e280fdf
Mikalai Varabei <7709215@gmail.com>
2022-03-18 14:34:18 +0300

    add factorial 0

--- src/homework/two/Factorial.java
+++ src/homework/two/Factorial.java
@@ -3,17 +3,18 @@ package homework.two;
 public class Factorial {
 
     public Long calculateFromCicle(Integer i){
-        if((i == null)||(i < 1)||(i > 10)){
+        if((i == null)||(i < 0)||(i > 10)){
             System.out.println("введено некорректное число");
             return null;
         }
+
+        if(i == 0) return 1L;
 
         int n = 1;
         int j = i;
         long fl = 1L;
 
         while (j > 0) {
-            //отсуствует проверка на переполнение, если нужно могу добавить
             fl = fl * n;
             n++;
             j--;
