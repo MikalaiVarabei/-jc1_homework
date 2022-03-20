3f07bdb1e4bed3a646584d5c9049de1c9f68e21a
Mikalai Varabei <7709215@gmail.com>
2022-03-20 10:32:04 +0300

    add task 15, 16

--- src/homework/two/Main.java
+++ src/homework/two/Main.java
@@ -15,9 +15,17 @@ public class Main {
         Digits sm = new Digits();
         System.out.println(sm.calcSumOfDigit(7_893_823_445L));
 
+        System.out.println("********** task15: **********");
+        Range num = new Range();
+        num.setRange(50, 70);
+        System.out.println(num.getPrimeNumber(2));
+
+        System.out.println("********** task16: **********");
+        num.setRange(1, 100);
+        System.out.println(num.getListDiv(7));
+
         System.out.println("********** task17: **********");
         System.out.println(sm.separationDigits(-11222333444555L));
-
     }
 
 }
