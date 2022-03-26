a769a48c9474544216458b3da39c9236569199f9
Mikalai Varabei <7709215@gmail.com>
2022-03-26 14:53:24 +0300

    add task 23, IntervalTime class

---
+++ src/homework/three/time/IntervalTime.java
@@ -0,0 +1,49 @@
+package homework.three.time;
+
+public class IntervalTime {
+    private final long secondsInterval;
+    private final long hours;
+    private final long minutes;
+    private final long seconds;
+
+    public IntervalTime(long secondsInterval) {
+        this.secondsInterval = secondsInterval;
+        this.hours = secondsInterval/3600;
+        this.minutes = (secondsInterval - hours*3600)/60;
+        this.seconds = secondsInterval - hours*3600 - minutes*60;
+    }
+
+    public IntervalTime(long seconds, long minutes, long hours) {
+        this.secondsInterval = seconds + minutes*60 + hours*3600;
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+    }
+
+    public long getSecondsInterval() {
+        return secondsInterval;
+    }
+
+    public long getSeconds(){
+        return seconds;
+    }
+
+    public long getMinutes(){
+        return minutes;
+    }
+
+    public long getHours(){
+        return hours;
+    }
+
+    public boolean compareTime(IntervalTime time){
+        return ((this.seconds == time.seconds)&&(this.minutes == time.minutes)&&(this.hours == time.hours));
+    }
+
+    public String getStrTime() {
+        String sec = (seconds > 10) ? ("" + seconds) : ("0"+seconds);
+        String min = (minutes > 10) ? ("" + minutes) : ("0"+minutes);
+        String time = hours+":"+min+":"+sec;
+        return time;
+    }
+}
---
+++ src/homework/three/time/Main.java
@@ -0,0 +1,14 @@
+package homework.three.time;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("********** task24: **********");
+        IntervalTime intervalTime_1 = new IntervalTime(127695);             //в конструктор передаем интервал в секундах
+        System.out.println(intervalTime_1.getStrTime());                                  //вывод данных
+        IntervalTime intervalTime_2 = new IntervalTime(15,28,35);   //в конструктор передаем часы, минуты, секунды
+        System.out.println(intervalTime_2.getStrTime());                                   //вывод данных
+        System.out.println(intervalTime_2.getSecondsInterval());                           //вывод интервала
+        System.out.println(intervalTime_1.compareTime(intervalTime_2));                    //сравниваем объекты
+    }
+}
