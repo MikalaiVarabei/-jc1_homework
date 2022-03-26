e0624f396f37b339a6a70c8eb1ebd73102935546
Mikalai Varabei <7709215@gmail.com>
2022-03-26 14:57:36 +0300

    correct IntervalTime class

--- src/homework/three/time/IntervalTime.java
+++ src/homework/three/time/IntervalTime.java
@@ -8,13 +8,13 @@ public class IntervalTime {
 
     public IntervalTime(long secondsInterval) {
         this.secondsInterval = secondsInterval;
-        this.hours = secondsInterval/3600;
-        this.minutes = (secondsInterval - hours*3600)/60;
-        this.seconds = secondsInterval - hours*3600 - minutes*60;
+        hours = secondsInterval/3600;
+        minutes = (secondsInterval - hours*3600)/60;
+        seconds = secondsInterval - hours*3600 - minutes*60;
     }
 
     public IntervalTime(long seconds, long minutes, long hours) {
-        this.secondsInterval = seconds + minutes*60 + hours*3600;
+        secondsInterval = seconds + minutes*60 + hours*3600;
         this.seconds = seconds;
         this.minutes = minutes;
         this.hours = hours;
