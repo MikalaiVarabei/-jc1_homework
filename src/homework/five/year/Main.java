abe0d5f4ca85caa0ffe33719ca5eb4932d4f2016
Mikalai Varabei <7709215@gmail.com>
2022-04-28 08:48:21 +0300

    add task 35

---
+++ src/homework/five/year/Main.java
@@ -0,0 +1,16 @@
+package homework.five.year;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Задание 35. Константы сезона года.");
+        System.out.println(SeasonYear.SPRING.toString());
+        System.out.println(SeasonYear.SUMMER.toString());
+        System.out.println(SeasonYear.AUTUMN.toString());
+        System.out.println(SeasonYear.WINTER.toString());
+
+
+
+
+    }
+}
